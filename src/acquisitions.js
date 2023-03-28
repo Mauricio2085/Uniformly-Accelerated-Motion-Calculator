52d3c5ecfee30d0cfd27d2be422c53ed79b9d51a
mauricio ocampo marulanda <maoca2085@gmail.com>
2023-03-28 13:17:41 -0500

    Creation index.js file and modifications in acquisitions.js and index.html

--- src/acquisitions.js
+++ src/acquisitions.js
@@ -1,52 +1,8 @@
 import Chart from 'chart.js/auto';
-
-const velO = document.getElementById('velO');
-const velF = document.getElementById('velF');
-const acel = document.getElementById('acel');
-const time = document.getElementById('time');
-const respuesta = document.getElementById('solution');
-const chartCanva = document.getElementById('acquisitions');
-const button = document.getElementById('cal')
-let distancia = [];
-const ArrayTime = [];
-
-const calcular = async () => {
-    const velONumber = parseFloat(velO.value);
-    const timeNumber = parseFloat(time.value);
-    const acelNumber = parseFloat(acel.value);
-    let subFormula1;
-    let subformula2;
-
-    
-    for(let index = 0; index <= timeNumber; index ++) {
-      let indexArr = index;
-      ArrayTime.push(indexArr);
-    }
-
-    console.log(ArrayTime);
-    ArrayTime.map(e => {
-      subformula1 = parseFloat(subFormula1 = e * velONumber);
-      console.log('formula 1', subFormula1);
-      subformula2 = parseFloat(subformula2 = 0.5 * acelNumber * e * e);
-      console.log('formula 2', subformula2);
-      distancia.push(subFormula1 + subformula2) ;
-      
-    })
-
-    console.log(distancia);
-    
-    respuesta.setAttribute('class', 'sol');
-    // respuesta.innerHTML = `<h1>La respuesta es: ${distancia.length}</h1>`;
-  
-    const g = await graph(distancia, ArrayTime);
-    
-}
-
-button.addEventListener('click', calcular)
-
-const labels = ArrayTime;
-
- const graph = async (distancia) => {
+ 
+ const graph = async (distancia, ArrayTime) => {
+  const chartCanva = document.getElementById('acquisitions');
+  const labels = ArrayTime;
   const data = distancia;// aqui voy
 
   new Chart(
@@ -57,7 +13,7 @@ const labels = ArrayTime;
         labels: labels,
         datasets: [
           {
-            label: 'Distancia recorrida',
+            label: 'Distancia recorrida cada segundo',
             data: data
           }
         ]
@@ -65,4 +21,6 @@ const labels = ArrayTime;
     }
   );
 };
+
+export {graph}; 
  
---
+++ src/index.js
@@ -0,0 +1,44 @@
+import Chart from 'chart.js/auto';
+import {graph} from './acquisitions';
+import saludar from './index'
+
+const velO = document.getElementById('velO');
+const velF = document.getElementById('velF');
+const acel = document.getElementById('acel');
+const time = document.getElementById('time');
+const respuesta = document.getElementById('solution');
+const button = document.getElementById('cal')
+let distancia = [];
+const ArrayTime = [];
+
+const calcular = async () => {
+    const velONumber = parseFloat(velO.value);
+    const timeNumber = parseFloat(time.value);
+    const acelNumber = parseFloat(acel.value);
+    let subFormula1;
+    let subformula2;
+
+    
+    for(let index = 0; index <= timeNumber; index ++) {
+      let indexArr = index;
+      ArrayTime.push(indexArr);
+    }
+
+    console.log('Array de intervalo de tiempo', ArrayTime);
+    ArrayTime.map(e => {
+      subformula1 = parseFloat(subFormula1 = e * velONumber);
+      subformula2 = parseFloat(subformula2 = 0.5 * acelNumber * e * e);
+      distancia.push(subFormula1 + subformula2) ;
+      
+    })
+
+    console.log('Array distancia', distancia);
+    
+    respuesta.setAttribute('class', 'sol');
+    // respuesta.innerHTML = `<h1>La respuesta es: ${distancia.length}</h1>`;
+  
+    const g = await graph(distancia, ArrayTime);
+    
+}
+
+button.addEventListener('click', calcular)
